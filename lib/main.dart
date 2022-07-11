5c717c93b84c8338e31aacb562511f17484462ef
thetechgirlgita <thetechgirlgita@gmail.com>
2022-07-11 16:01:43 +0545

    Formatting

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'constant.dart';
 void main() {
   runApp(const Weather());
 }
+
 class Weather extends StatefulWidget {
   const Weather({Key? key}) : super(key: key);
 
@@ -33,25 +34,32 @@ class weather extends StatefulWidget {
 class _weatherState extends State<weather> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      backgroundColor: backgroundC,
-      body:
-         Row(
-           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: const [
+    return Scaffold(
+        backgroundColor: backgroundC,
+        body: Column(
+          children: [
+            Row(
+                children: [
+                  Container(
+                    color: Colors.black,
+                    height: MediaQuery.of(context).size.height*0.05,
+                    width: MediaQuery.of(context).size.width,
+                  ),
 
-            Text("Weather Forecast",
-              style: TextStyle(
-                color : Colors.white,
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-              )
-            ),
+                ]),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text("Weather Forecast",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    )),
+              ],
+            )
           ],
-        )
-    );
+        ));
   }
 }
-
-
