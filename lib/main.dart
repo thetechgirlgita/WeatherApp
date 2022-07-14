2fe736e823f8f7a3e9881514f9f8f09496079143
thetechgirlgita <thetechgirlgita@gmail.com>
2022-07-14 19:02:00 +0545

    Adding Search Bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hexcolor/hexcolor.dart';
+
 import 'constant.dart';
 
 void main() {
@@ -36,30 +36,135 @@ class _weatherState extends State<weather> {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: backgroundC,
+        appBar: AppBar(
+          backgroundColor: appbarC,
+          centerTitle: true,
+          title:  const Text("Weather Forecast",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              )),
+        ),
         body: Column(
           children: [
             Row(
-                children: [
-                  Container(
-                    color: Colors.black,
-                    height: MediaQuery.of(context).size.height*0.05,
-                    width: MediaQuery.of(context).size.width,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children:  [
+                Padding(
+                  child: IconButton(
+                      onPressed: (){
+                        showSearch(
+                            context: context,
+                            delegate: ListOfCountries(),
+                        );
+                      },
+                      icon:  const Icon(Icons.location_pin,
+                        color: Colors.yellow,
+                        size: 35,
+                      ),
                   ),
 
-                ]),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [
-                Text("Weather Forecast",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                    )),
+                  padding: const EdgeInsets.only(top: 10),
+                ),
+                const Text("Delhi",
+                      style: TextStyle(
+                        fontSize: 25,
+                        color:  Colors.white,
+                        fontWeight: FontWeight.bold,
+                      )
+                  ),
+
               ],
             )
           ],
         ));
   }
 }
+class ListOfCountries extends SearchDelegate {
+// Demo list to show querying
+  List<String> searchTerms = [
+    "India",
+    "Nepal",
+    "Maldives",
+    "USA",
+    "Canada",
+    "China",
+    "Russia",
+    "Srilanka",
+    "IreLand",
+    "Newzealand",
+    "Japan",
+    "Germany",
+    "France",
+    "Paris"
+  ];
+
+// first overwrite to
+// clear the search text
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        onPressed: () {
+          query = '';
+        },
+        icon: const Icon(Icons.clear),
+      ),
+    ];
+  }
+
+// second overwrite to pop out of search menu
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        close(context, null);
+      },
+      icon: const Icon(Icons.arrow_back),
+    );
+  }
+
+// third overwrite to show query result
+  @override
+  Widget buildResults(BuildContext context) {
+    List<String> matchQuery = [];
+    for (var country in searchTerms) {
+      if (country.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(country);
+      }
+    }
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return ListTile(
+          title: Text(result),
+        );
+      },
+    );
+  }
+
+// last overwrite to show the
+// querying process at the runtime
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> matchQuery = [];
+    for (var fruit in searchTerms) {
+      if (fruit.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(fruit);
+      }
+    }
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return ListTile(
+          title: Text(result),
+        );
+      },
+    );
+  }
+}
+
